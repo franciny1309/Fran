5b8a12e1ff50063aea36c694cb6c2349bd8375cd
LAB01 <LAB01@PC-14>
2020-01-24 19:50:03 -0600

    Prueba

--- Recursividad/src/main/java/MainClases.java
+++ Recursividad/src/main/java/MainClases.java
@@ -1,8 +1,11 @@
 
 public class MainClases {
 
-    private static void DoJob() {
-        Recuersivida.
+     private static void DoJob() {
+
+        Recursividad ejemploRecursivo = 
+                new Recursividad ();
+        ejemploRecursivo.EjecutarEjemploUno();
     }
 
  public static void main(String[] args) {
